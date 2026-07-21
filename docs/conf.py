1ce82bc7b2042321f84fcfcfcbe276dd44c58846
Afonso Silva <ajcerejeira@gmail.com>
2026-07-21 18:55:24 +0100

    Use uv for readthedocs builder

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# Project information
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""Configuration file for the Sphinx documentation builder."""
 
 from datetime import date
 from importlib.metadata import metadata
